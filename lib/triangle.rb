dfff9ae95b04a9101fbe169254b850299e731af9
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-24 23:41:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     one = self.side_one
     two = self.side_two
     three = self.side_three
-    if (one, two, three) =~
+    if (one, two, three) =~ /[1-9]{3}/
       return :equilateral
     elsif one == two || one == three || two == three
       return :isosceles
